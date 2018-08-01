57f1d6924fc1383e2f4bc40d7fcbb8cff14ae977
Anar <haqverdiyevanar@gmail.com>
2018-08-01 17:07:02 +0400

    Update weather.js

--- weather.js
+++ weather.js
@@ -4,7 +4,7 @@ class Weather{
         this.key = '3474e7402d8183780e55df52a85c920d'
     }
     async getWeather(){
-        const response = await fetch(`http://api.apixu.com/v1/forecast.json?key=fe264698e50c40078aa82829180106&q=${this.city}&days=5`)
+        const response = await fetch(`https://api.apixu.com/v1/forecast.json?key=fe264698e50c40078aa82829180106&q=${this.city}&days=5`)
         return await response.json();
     }
     changeLocation(city){
